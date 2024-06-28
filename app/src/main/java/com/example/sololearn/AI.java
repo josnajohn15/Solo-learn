66c5e59347f4e76c863dda125de825964abcc63a
josnajohn15 <josnajohn153@gmail.com>
2024-06-28 10:12:02 +0530

    project created

---
+++ app/src/main/java/com/example/sololearn/AI.java
@@ -0,0 +1,54 @@
+package com.example.sololearn;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class AI extends AppCompatActivity {
+    EditText ed1,ed2,ed3;
+    AppCompatButton b1,b2;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_ai);
+        ed1=(EditText) findViewById(R.id.studid);
+        ed2=(EditText) findViewById(R.id.name);
+        ed3=(EditText) findViewById(R.id.cname);
+        b1=(AppCompatButton) findViewById(R.id.enroll);
+        b2=(AppCompatButton) findViewById(R.id.back);
+
+
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String getStudId=ed1.getText().toString();
+                String getStudName=ed2.getText().toString();
+                String getCollege=ed3.getText().toString();
+                Toast.makeText(getApplicationContext(),getStudId+" "+getStudName+" "+getCollege,Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(i);
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/sololearn/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.sololearn;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    AppCompatButton b1,b2,b3,b4,b5;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        b1=(AppCompatButton) findViewById(R.id.c);
+        b2=(AppCompatButton) findViewById(R.id.p);
+        b3=(AppCompatButton) findViewById(R.id.w);
+        b4=(AppCompatButton) findViewById(R.id.f);
+        b5=(AppCompatButton) findViewById(R.id.g);
+
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(getApplicationContext(),CProgram.class);
+                startActivity(i);
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(getApplicationContext(),Python.class);
+                startActivity(i);
+            }
+        });
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(getApplicationContext(),Web.class);
+                startActivity(i);
+            }
+        });
+        b4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(getApplicationContext(),FrontEnd.class);
+                startActivity(i);
+            }
+        });
+        b5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(getApplicationContext(),AI.class);
+                startActivity(i);
+            }
+        });
+
+
+
+
+
+
+    }
+}
